aa4e5466d1f10f477b50753f8a2564e41faaa1f7
Keshab Kumar <keshabkumarjha876@gmail.com>
2025-07-10 15:23:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.java
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.java
@@ -1,55 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-class GFG {
-    static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t > 0) {
-            String str[] = read.readLine().trim().split(" ");
-            int n = str.length;
-            Node head = new Node(Integer.parseInt(str[0]));
-            Node tail = head;
-
-            for (int i = 1; i < n; i++) {
-                tail.next = new Node(Integer.parseInt(str[i]));
-                tail = tail.next;
-            }
-
-            Solution ob = new Solution();
-            head = ob.reverseList(head);
-            printList(head);
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // function Template for Java
 
 /* linked list node class:
@@ -67,17 +15,15 @@ class Node {
 class Solution {
     Node reverseList(Node head) {
         // code here
-        Node h = head ;
-        Node d = head;
-        ArrayList<Integer> al = new ArrayList<>();
-        while(h!=null){
-            al.add(h.data);
-            h = h.next;
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while(curr!= null){
+            next = curr.next;
+            curr.next= prev;
+            prev = curr;
+            curr = next;
         }
-        for(int i = al.size()-1;i>-1;i--){
-            head.data = al.get(i);
-            head = head.next;
-        }
-        return d;
+        return prev;
     }
 }
